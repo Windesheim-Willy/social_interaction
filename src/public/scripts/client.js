9e8083f7558fae39ac412c3e838ed4a0427c7aea
Fabian van de Bor <info@fabianvdbor.nl>
2018-11-20 16:29:49 +0100

    Remove unused code

--- src/public/scripts/client.js
+++ src/public/scripts/client.js
@@ -1,28 +1,6 @@
 $(document).ready(function () {
     var socket = io.connect(window.location.protocol + '//' + window.location.hostname + ':' + window.location.port);
 
-    socket.on('rosIsActive', function (data) {
-        var $body = $('body');
-
-        // Change the body class if ROS is active or not.
-        if (data.is_active) {
-            $body.addClass('ros-is-active')
-                .removeClass('ros-is-not-active');
-        }
-        else {
-            $body.removeClass('ros-is-active')
-                .addClass('ros-is-not-active');
-        }
-
-        if (data.additional_content) {
-            var $content = $('#additional-content');
-
-            // $content.hide();
-            $content.html(data.additional_content);
-            // $content.show("slow");
-        }
-    });
-
     /**
      * Socket event for changing the mood of Willy.
      */
