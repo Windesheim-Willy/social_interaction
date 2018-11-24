7094493278f575d399b08a00c73ab28089679648
Fabian van de Bor <info@fabianvdbor.nl>
2018-11-24 15:09:38 +0100

    Update start script

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd src
-
-source /opt/ros/kinetic/setup.bash
-export ROS_PACKAGE_PATH="$(pwd)/../:$ROS_PACKAGE_PATH"
-
-sudo npm install
-sudo npm run sass:build
-rosrun social_interaction start.sh
