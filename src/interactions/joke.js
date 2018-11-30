70d5e3a2e6fe8e5d6757aafa49703011b204488a
Fabian van de Bor <info@fabianvdbor.nl>
2018-11-30 20:14:50 +0100

    Create joke interaction

---
+++ src/interactions/joke.js
@@ -0,0 +1,60 @@
+const interactionBase = require('./interactionBase');
+const pug = require('pug');
+const fs = require('fs');
+const path = require('path');
+const screenSize = require('../config/screenSize');
+const rosConnection = require('../adapters/rosConnection');
+
+class joke extends interactionBase {
+
+    /**
+     * Constructor of interaction base.
+     * @param io socket connection.
+     */
+    constructor(io) {
+        super(io);
+        this.jokes = JSON.parse(fs.readFileSync(path.join(__dirname, 'assets', 'jokes.json')));
+
+    }
+
+    /**
+     * If this interaction should activate on this text input.
+     * @param text
+     * @returns {boolean}
+     */
+    activateOnInput(text) {
+        var regex = /(grap|mop)/i;
+        return text.match(regex);
+    }
+
+
+    /**
+     * Active this interaction.
+     */
+    activate() {
+        super.activate();
+        var io = this.io;
+
+        // Change the frontend of Willy.
+        io.emit('changeFormat', screenSize.large);
+
+        var joke = this.jokes[Math.floor(Math.random() * this.jokes.length)];
+
+        var content = pug.renderFile('views/information.pug', {
+            h1: joke,
+        });
+        io.emit('changeContent', content);
+        io.emit('confetti', true);
+
+        // Speak the information about the map.
+        // @TODO: speak the information.
+
+        setTimeout(function () {
+            rosConnection.changeRosActive(0);
+            io.emit('confetti', false);
+        }, 10000);
+    }
+
+}
+
+module.exports = joke;
